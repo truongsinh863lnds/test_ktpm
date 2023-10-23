d56faaac45879a5c6e6e3249e866cc863535b89d
truongsinh863lnds <2151013080sinh@ou.edu.vn>
2023-10-23 14:10:38 +0700

    so 89 them file thong bao moi vao nhanh branch_Sinh

--- thongbao.cpp
+++ thongbao.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
+
+int sum_array(int[] a, int n){
+    int sum = 0;
+    for (int i = 0; i <n; i++)
+    {
+         sum += a[i];
+    }
+    return sum;
+}
+
+
+
 void main{
-    cout<<"Hello world";
+    cout << "Hello world" << endl;
+
 }
