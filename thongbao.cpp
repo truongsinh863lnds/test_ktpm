95496c96211a73225611d20acc87f1dfc9179a51
ThangTB5 <2151010348thang@ou.edu.vn>
2023-10-23 14:10:57 +0700

    so 56 them  thong bao moi vao nhanh feature-thang

--- thongbao.cpp
+++ thongbao.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
+
+int sum_array(int[] a, int n) {
+    int sum = 0;
+    for (int i = 0; i <= n; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
+
 void main{
     cout<<"Hello world";
+    
 }
